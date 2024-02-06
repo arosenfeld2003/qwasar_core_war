1da471636b05f17a7d094efde5044b3f1b35357d
Tony Bronzo <3690643+thetone@users.noreply.github.com>
2024-02-06 12:29:27 -0600

    asm: added writing to binary file
    live is the only command that works right now

--- asm/asm.c
+++ asm/asm.c
@@ -14,6 +14,11 @@
 #define LDELIMS " \t\n"         /* label delims */
 #define OPDELIMS " \t\n,"       /* operand delims */
 
+// TEMP OP codes; use op.h
+#define OP_LIVE 1
+#define OP_LD 2
+#define OP_ST 3
+
 typedef struct {
     char *label;
     char *instruction;
@@ -21,9 +26,11 @@ typedef struct {
     int operand_count;
 } inst_t;
 
-void parse_program(FILE *input_file);
+void parse_program(FILE *input_file, FILE *output_file);
 void parse_line(char *line, inst_t *inst);
 void output_inst(inst_t *inst);
+void write_instruction(FILE *output_file, inst_t *inst);
+int get_opcode(inst_t *inst);
 
 int main(int argc, char **argv)
 {
@@ -40,7 +47,7 @@ int main(int argc, char **argv)
     }
     
     // create output file
-    FILE *output_file = fopen("a.cor", "w"); // overwrites is file exists
+    FILE *output_file = fopen("a.cor", "w"); // overwrites if file exists
     if (output_file == NULL) {
         perror("Error creating output file"); 
         return 1;
@@ -48,7 +55,7 @@ int main(int argc, char **argv)
 
     // TODO: write header for output
 
-    parse_program(input_file);
+    parse_program(input_file, output_file);
 
     fclose(input_file);
     fclose(output_file);
@@ -56,7 +63,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void parse_program(FILE *input_file)
+void parse_program(FILE *input_file, FILE *output_file)
 {
     char line[MAX_LINE_LENGTH];
 
@@ -64,10 +71,10 @@ void parse_program(FILE *input_file)
         inst_t inst = {};
 
         // TEST
-        printf("Line from file: %s", line); // no \n bc line has it
+        //printf("Line from file: %s", line); // no \n bc line has it
 
         parse_line(line, &inst);
-        //write_inst()  // TODO
+        write_instruction(output_file, &inst);
         //free_inst() // TODO
     }
 }
@@ -95,8 +102,54 @@ void parse_line(char *line, inst_t *inst)
         }
     }
     // TEST
-    output_inst(inst);
-    printf("*********\n");
+    //output_inst(inst);
+    //printf("*********\n");
+}
+
+void write_instruction(FILE *output_file, inst_t *inst)
+{
+    int opcode;  /* in base 10 despite encoded in hex */
+    int param;              /* value of a parameter */
+
+    // set opcode based on instruction
+    opcode = get_opcode(inst);
+
+    // write to file?
+    fwrite(&opcode, sizeof(opcode), 1, output_file);
+
+    switch (opcode) {  // TODO refactor to functions
+        case OP_LIVE:
+            param = atoi(inst->operands[0] + 1); /* bypass % */
+            fwrite(&param, sizeof(param), 1, output_file);
+            break;
+        case OP_LD:
+        case OP_ST:       // TODO ST is similar but needs r check on 1st param
+            // handle first param
+            if (inst->operands[0][0] == '%') {
+                param = atoi(inst->operands[0] + 1); /* bypass * % */
+                fwrite(&param, sizeof(param), 1, output_file);
+            }
+            else {
+                param = atoi(inst->operands[0]);    /* indirect value */
+                fwrite(&param, sizeof(param), 1, output_file);
+            }
+            // handle 2nd param
+            fwrite(&(inst->operands[1][1]), 1, 1, output_file);  /* skips 'r' */
+            break;
+        default:
+            return;     
+    }
+}
+
+int get_opcode(inst_t *inst)
+{
+    if (strcmp(inst->instruction, "live") == 0)
+        return OP_LIVE;
+    if (strcmp(inst->instruction, "ld") == 0)
+        return OP_LD;
+    if (strcmp(inst->instruction, "st") == 0)
+        return OP_ST;
+    return -1;       // bad instruction
 }
 
 void output_inst(inst_t *inst)
