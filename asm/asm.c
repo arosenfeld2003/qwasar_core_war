bc300e9e35e7cc66d36e85c21758584640c55554
Tony Bronzo <3690643+thetone@users.noreply.github.com>
2024-03-01 15:18:03 -0600

    added name/comment parsing

--- asm/asm.c
+++ asm/asm.c
@@ -20,6 +20,8 @@
 #define REG_BIN 0b01
 #define DIR_BIN 0b10
 #define IND_BIN 0b11
+#define HEADER_NAME ".name"
+#define HEADER_COMMENT ".comment"
 
 typedef struct {
     char *label;
@@ -28,13 +30,19 @@ typedef struct {
     int arg_count;           /* operand count */
 } inst_t;
 
-void parse_program(FILE *input_file, FILE *output_file);
+typedef struct {
+    char *name;
+    char *comment;
+} head_t;
+
+void parse_program(int input_fd, int output_fd, head_t *head_ptr);
 void parse_line(char *line, inst_t *inst);
 void output_inst(inst_t *inst);
 void write_instruction(FILE *output_file, inst_t *inst);
 int get_opcode(inst_t *inst);
 int make_valid_ptype_byte(inst_t *inst, int opcode);
 int is_valid_label(char *label);
+int has_arg_types(char *inst_name);
 
 int main(int argc, char **argv)
 {
@@ -57,8 +65,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // TODO: write header for output
-
     parse_program(input_file, output_file);
 
     fclose(input_file);
@@ -67,25 +73,28 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void parse_program(FILE *input_file, FILE *output_file)
+void parse_program(int input_fd, int output_fd, head_t *head_ptr)
 {
     char line[MAX_LINE_LENGTH];
     int offset = 0;                         /* for label byte offset */
     htable **ht_labels;                      /* table for labels */
+    head_t header = {};
 
     while (fgets(line, MAX_LINE_LENGTH, input_file)) { /* first pass */
         inst_t inst = {};
 
-        parse_line(line, &inst, 1, &offset, ht_labels);
+        parse_line(line, &inst, 1, &offset, ht_labels, &header);
         //free_inst() // TODO
     }
 
     lseek(input_fd, 0, SEEK_SET);  /* reset to top of file */
+    write(output_fd, head_ptr->name, my_strlen(head_ptr->name));
+    write(output_fd, head_ptr->comment, my_strlen(head_ptr->comment));
 
     while (fgets(line, MAX_LINE_LENGTH, input_file)) { /* second pass */
         inst_t inst = {};
 
-        parse_line(line, &inst, 1, &offset, ht_labels);
+        parse_line(line, &inst, 1, &offset, ht_labels, &header);
         write_instruction(output_file, &inst, ht_labels);
         //free_inst() // TODO
     }
@@ -93,25 +102,35 @@ void parse_program(FILE *input_file, FILE *output_file)
     
 }
 
-void parse_line(char *line, inst_t *inst, int pass_num, int *offset, htable **ht_labels)
+void parse_line(char *line, inst_t *inst, int pass_num, int *offset, htable **ht_labels, head_t *head_ptr)
 {
     char *token;
     int table_index, num_args;
     char *saveptr;  // required by strtok_r to use internally
 
-    // TODO parse .name/.comment
-
     token = strtok_r(line, LDELIMS, &saveptr); // returns ptr to 1st token
 
+    // parse .name/.comment
+    if (my_strcmp(token, HEADER_NAME) == 0) {
+        token = strtok_r(NULL, LDELIMS, &saveptr); // get next item
+        my_strdup(head_ptr->name, token);
+        token = strtok_r(NULL, LDELIMS, &saveptr);
+    }
+    else if (my_strcmp(token, HEADER_COMMENT) == 0) {
+        token = strtok_r(NULL, LDELIMS, &saveptr);
+        my_strdup(head_ptr->comment, token);
+        token = strtok_r(NULL, LDELIMS, &saveptr);
+    }
     
     // check for label
-    if (token && token[strlen(token) - 1] == ':') { // label found
-        //inst->label = strdup(token);  // store label name
+    if (token && token[strlen(token) - 1] == LABEL_CHAR) {
         if (pass_num == 1) {
             if (!is_valid_label(token))
                 exit(2);
             ht_add(ht_labels, token, *offset);
         }
+        else
+            inst->label = strdup(token);  // store label name
         token = strtok_r(NULL, LDELIMS, &saveptr); // pass NULL to continue line
     }
 
@@ -125,11 +144,11 @@ void parse_line(char *line, inst_t *inst, int pass_num, int *offset, htable **ht
                 && inst->operand_count < num_args) {
             if (pass_num == 1) {
                 (*offset)++;    /* instruction byte */
-                if (has_arg_types(inst->name))   // TODO make function
+                if (has_arg_types(inst->name))
                     (*offset)++;            /* ptype byte */
                 switch (inst->args[inst->arg_count][0]) {
-                    case 'r': *offset += REG_SIZE; break;
-                    case '%': *offset += DIR_SIZE; break;
+                    case REG_CHAR: *offset += REG_SIZE; break;
+                    case DIRECT_CHAR: *offset += DIR_SIZE; break;
                     default:  *offset += IND_SIZE;
                 }
             }
@@ -157,17 +176,18 @@ int write_instruction(FILE *output_file, inst_t *inst, htable **ht_labels)
 
     /* write to file */
     write(output_fd, &opcode, 1);
-    write(output_fd, &ptype_byte, 1);
+    if (has_arg_types(inst->name))
+        write(output_fd, &ptype_byte, 1);
 
     for (int i = 0; i < inst->arg_count; i++) {
         int arg, offset = 1;
 
         switch (inst->args[i][0]) {
-            case 'r':
+            case REG_CHAR:
                 arg = my_atoi(inst->args[i] + offset);   /* +1 for 'r' */
                 write(output_fd, &arg, REG_SIZE);
                 break;
-            case '%':
+            case DIRECT_CHAR:
                 if (inst->args[i][1] && inst->args[i][1] == LABEL_CHAR) {
                     int index = ht_find_index(ht_labels, &inst->args[i][2]);
                     if (index == -1)
@@ -178,7 +198,7 @@ int write_instruction(FILE *output_file, inst_t *inst, htable **ht_labels)
                     arg = my_atoi(inst->args[i] + offset);   /* +1 for '%' */
                 write(output_fd, &arg, DIR_SIZE);
                 break;
-            case ':':           /* indirect label */
+            case LABEL_CHAR:           /* indirect label */
                 int index = ht_find_index(ht_labels, inst->args[i][1]);
                 if (index == -1)
                     exit(1);
@@ -272,4 +292,17 @@ int is_valid_label(char *label)
             return 0;
     }
     return 1;
+}
+
+/*
+ * purpose: used to determine if instruction needs ptypes byte
+ */
+int has_arg_types(char *inst_name)
+{
+    return (
+        my_strcmp(inst_name, "live") != 0 &&
+        my_strcmp(inst_name, "zjmp") != 0 &&
+        my_strcmp(inst_name, "fork") != 0 &&
+        my_strcmp(inst_name, "lfork") != 0 &&
+    );
 }
